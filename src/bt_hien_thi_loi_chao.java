e67be1f89931d94a14203f246402cac12b89616c
dinhkimanh123 <118419564+dinhkimanh123@users.noreply.github.com>
2023-02-08 15:13:57 +0700

    Add files via upload

---
+++ src/bt_hien_thi_loi_chao.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class bt_hien_thi_loi_chao {
+    public static void main (String[] arge) {
+      Scanner sc= new Scanner(System.in);
+        System.out.println("Nhạp ten cua ban ");
+       String name = sc.nextLine();
+        System.out.println("Hello");
+
+    }
+
+}
