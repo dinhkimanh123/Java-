6cda942678e6e9299277a8f0ddb533b7466d444a
dinhkimanh123 <118419564+dinhkimanh123@users.noreply.github.com>
2023-02-08 10:30:14 +0700

    Add files via upload

---
+++ src/khaibao_sudungbien.java
@@ -0,0 +1,18 @@
+public class khaibao_sudungbien {
+    public static void main(String[] args) {
+        int i=10;
+        float number = 20.5F;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s= "Hà nội";
+        System.out.println("i=" + i);
+        System.out.println("number=" + number);
+        System.out.println("d=" + d);
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+        System.out.println("s=" + s);
+
+
+    }
+}
