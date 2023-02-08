eecfae69e5352ef4ab164999659a370f1502c7c0
dinhkimanh123 <118419564+dinhkimanh123@users.noreply.github.com>
2023-02-08 09:53:23 +0700

    Add files via upload

---
+++ src/thoigianhethong.java
@@ -0,0 +1,10 @@
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class thoigianhethong {
+    public static void main (String[] args) {
+        Date now = new Date(); {
+            System.out.printf("Date now:" + now);
+        }
+    }
+}
