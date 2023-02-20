c39964f0c1e273945c527e5f0f6536ff510b1249
dinhkimanh123 <118419564+dinhkimanh123@users.noreply.github.com>
2023-02-20 08:49:42 +0700

    Add files via upload

---
+++ hoc_them_vong_lap/src/bai_2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class bai_2 {
+    public static void main(String[] args) {
+//
+        int i = 0;
+
+//
+        for (i = 0; i < 10; i++) {
+            if (i == 3 || i == 7) {
+                System.out.println("");
+            } else if (i % 2 == 1) {
+                System.out.println(i + "là số lẻ ");
+
+            }
+        }
+    }
+}
+
+
