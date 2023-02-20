48ec2615b1590e160406718eb462fabd8170b921
dinhkimanh123 <118419564+dinhkimanh123@users.noreply.github.com>
2023-02-20 08:50:15 +0700

    Add files via upload

---
+++ hoc_them_vong_lap/src/bai_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class bai_3 {
+    public static void main(String[] args) {
+//
+        int i = 0;
+
+//
+        for (i = 0; i < 10; i++) {
+            if (i == 4) {
+                System.out.println("");
+            } else if (i % 2 == 0) {
+                System.out.println(i + "là số chẵn ");
+
+            }
+        }
+    }
+}
